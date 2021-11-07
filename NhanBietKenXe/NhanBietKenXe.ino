497317c28f894aa944efc79353385c839e27e703
Trung Nguyen Thien <ngthientrung@gmail.com>
2021-11-07 18:12:26 +0700

    no message

--- NhanBietKenXe/NhanBietKenXe.ino
+++ NhanBietKenXe/NhanBietKenXe.ino
@@ -12,12 +12,13 @@
 #define TRACKING_STATUS_NOT_TRACKING 0
 #define TRACKING_STATUS_NEED_START_TRACKING 1
 #define TRACKING_STATUS_TRACKING 2
-#define HONK_MIN 100
+#define HONK_MIN 250
 
 int soundPins[SOUND_PIN_TOTAL] = {A0, A1};
 int soundValues[SOUND_PIN_TOTAL][MAX_SOUND_TRACK];
 SMSGSM sms;
-boolean started = false; //trạng thái modul sim
+boolean sim900a_started = false; //trạng thái module sim
+char tempt[50];
 
 void reset_sound_value() {
   for (int p = 0; p < SOUND_PIN_TOTAL; p++) {
@@ -27,7 +28,7 @@ void reset_sound_value() {
   }
 }
 
-int *read_sound_array() {
+int *read_sound_pins() {
   int *values = new int[SOUND_PIN_TOTAL];
   for (int p = 0; p < SOUND_PIN_TOTAL; p++) {
     int pin = soundPins[p];
@@ -37,8 +38,7 @@ int *read_sound_array() {
   return values;
 }
 
-
-int null_soundtrack_index() {
+int get_unset_index() {
   for (int t = 0; t < MAX_SOUND_TRACK; t++) {
     if (soundValues[0][t] < 0) return t;
   }
@@ -47,17 +47,9 @@ int null_soundtrack_index() {
 }
 
 void set_newSoundTrack(int trackIndex, int *newValues) {
-  // Serial.println("");
-  // Serial.print("trackIndex: ");
-  // Serial.print(trackIndex);
-  // Serial.println("");
-  // Serial.print("newSoundTrack: ");
   for (int p = 0; p < SOUND_PIN_TOTAL; p++) {
     int value = *(newValues + p);
     soundValues[p][trackIndex] = value;
-
-    // Serial.print(value);
-    // Serial.print(" ");
   }
 }
 
@@ -68,7 +60,6 @@ int tracking_status(int *newValues) {
   int hasHonkValue = false;
   for (int p = 0; p < SOUND_PIN_TOTAL; p++) {
     int value = *(newValues + p);
-    // print_sound_bar(p, value);
     if (value >= HONK_MIN) {
       hasHonkValue = true;
       break;
@@ -80,7 +71,8 @@ int tracking_status(int *newValues) {
 }
 
 bool need_start_analyse() {
-  return soundValues[0][MAX_SOUND_TRACK - 1] >= 0;
+  int last_sound_index = MAX_SOUND_TRACK - 1;
+  return soundValues[0][last_sound_index] >= 0;
 }
 
 int read_sound(int si) {
@@ -88,38 +80,28 @@ int read_sound(int si) {
   return analogRead(pin);
 }
 
-void print_sound_bar(int si, int value) {
-  int bar = int(value / 10);
-  Serial.println("");
-  Serial.print("A");
-  Serial.print(si);
-  Serial.print(":");
-  for (int bi = 0; bi < bar; bi++) {
-    Serial.print("x");
-  }
-  Serial.print(value);
-}
-
 void print_all() {
   for (int p = 0; p < SOUND_PIN_TOTAL; p++) {
-    Serial.println("");
-    Serial.print("A");
-    Serial.print(p);
-    Serial.print(":");
+    sprintf(tempt, "A%d:", p);
+    Serial.print(tempt);
+
     for (int t = 0; t < MAX_SOUND_TRACK; t++) {
       Serial.print(soundValues[p][t]);
       Serial.print(",");
+    }
+    if(p < SOUND_PIN_TOTAL - 1) {
+      Serial.print("\n");
     }
   }
 }
 
 void do_analyse() {
-  Serial.println("");
-  Serial.print("DO ANALYSE ============  ");
+  Serial.println("\n ============ DO ANALYSE ============  ");
   print_all();
   int max = -1;
   int soundMax = -1;
-  for (int p = 0; p < 2; p++) {
+
+  for (int p = 0; p < SOUND_PIN_TOTAL; p++) {
     for (int t = 0; t < MAX_SOUND_TRACK; t++) {
       int value = soundValues[p][t];
       if (value > max) {
@@ -128,20 +110,27 @@ void do_analyse() {
       }
     }
   }
-  Serial.println("");
-  Serial.print("SOUND AT ");
-  Serial.print(soundMax);
-  Serial.println("");
+  sprintf(tempt, "\nMax at: %d", soundMax);
+  Serial.println(tempt);
+
   sendSMS(soundMax);
 }
 
-void sendSMS(int soundIndex) {
-  if (started) {
-    char sdt[] = "+84377670064";
-    char msg[20];
-    sprintf(msg, "From %d", soundIndex);
-    //sms.SendSMS(sdt, msg);
+void setup_module_sim900a() { // Call at setup()
+  if (gsm.begin(2400)) {
+    Serial.println("\nsim900a: READY");
+    sim900a_started = true;
+  } else {
+    Serial.println("\nsim900a: IDLE");
   }
+}
+
+void sendSMS(int soundIndex) {
+  if (!sim900a_started) return;
+
+  char phone_number[] = "+84377670064";
+  sprintf(tempt, "Sound from sensor at %d", soundIndex);
+  // sms.SendSMS(phone_number, tempt);
 }
 
 ///////////////////////////////////////////////
@@ -149,11 +138,7 @@ void sendSMS(int soundIndex) {
 void setup() {
   Serial.begin(9600);
   reset_sound_value();
-
-  if (gsm.begin(2400)) {
-    Serial.println("\nstatus=READY");
-    started = true;
-  } else Serial.println("\nstatus=IDLE");
+  setup_module_sim900a();
 }
 
 void loop() {
@@ -162,20 +147,13 @@ void loop() {
     reset_sound_value();
   }
 
-  int *sounds = read_sound_array();
+  int *sounds = read_sound_pins();
   int status = tracking_status(sounds);
 
-  // Serial.println("");
-  // Serial.print("STATUS  ");
-  // Serial.print(status);
-  // Serial.println("");
   if (status == TRACKING_STATUS_TRACKING || status == TRACKING_STATUS_NEED_START_TRACKING) {
-    int nullIndex = null_soundtrack_index();
-    // Serial.println("nullIndex = ");
-    // Serial.print(nullIndex);
+    int nullIndex = get_unset_index();
     set_newSoundTrack(nullIndex, sounds);
   }
-
 
   delay(DELAY_TIME);
 }
